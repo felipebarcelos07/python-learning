612c34069918361abc4c4ff570e85dc7b38800d8
felipebarcelos07 <felipebarcelos07@gmail.com>
2026-03-09 14:20:35 -0300

    Update averagegrade.py

--- averagegrade.py
+++ averagegrade.py
@@ -1,9 +1,13 @@
 n1 = float(input('Enter your first grade: '))
+
 n2 = float(input('Enter your second grade: '))
+
 m = (n1 + n2) / 2
+
 if m >= 6:
     print('You passed! CONGRATS!!')
 else:
     print('You failed. KEEP STUDYING!')
 print(f"Your average grade is: {m:.1f}")
+
 
