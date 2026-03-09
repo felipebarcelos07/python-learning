fbe2dd75e764cf3fc1ceb749180955efcd2dbfc1
felipebarcelos07 <felipebarcelos07@gmail.com>
2026-03-09 13:44:33 -0300

    Add files via upload

---
+++ averagegrade.py
@@ -0,0 +1,9 @@
+
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+m = (n1 + n2) / 2
+if m >= 6:
+    print('Você passou de ano! PARABÉNS!!')
+else:
+    print('Você foi reprovado, ESTUDE MAIS!')
+print("Sua média foi de : ", m)
